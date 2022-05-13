5d42279c0854f253edb48dbf8eb833613e22a122
Felipe Sousa <felipe.fps09@hotmail.com>
2022-05-12 22:30:41 -0300

    MovieDTO

--- src/main/java/com/devsuperior/movieflix/entities/Review.java
+++ src/main/java/com/devsuperior/movieflix/entities/Review.java
@@ -3,10 +3,22 @@ package com.devsuperior.movieflix.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_review")
 public class Review implements Serializable{
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	
 	private String text;
 	private User user;
 	private Movie movie;
--- src/main/java/com/devsuperior/movieflix/entities/User.java
+++ src/main/java/com/devsuperior/movieflix/entities/User.java
@@ -7,14 +7,28 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String password;
 
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_user_role", 
+	joinColumns = @JoinColumn(name = "user_id"),
+	inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
 
 	private List<Review> reviews = new ArrayList<>();
